433e766179130a50d348d8678f1851769f4c39ac
42hub <63877490+42hub@users.noreply.github.com>
2021-04-25 13:23:08 +0200

    1

--- script.sh
+++ script.sh
@@ -1,1 +1,15 @@
-say hi
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install python3
+
+eval "$/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+
+brew install docker
+
+
+brew install python3
+
+sudo apt install python3-pip
+
+
+pip3 install wilfred --upgrade
